267bf23f6d3fd70920a64ec76f1a7ee8246db1a6
Tal Finkelshtein <finkelshtein.tal@gmail.com>
2025-02-11 16:53:11 +0200

    font and styling

--- src/main.tsx
+++ src/main.tsx
@@ -3,17 +3,8 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import { GoogleOAuthProvider } from "@react-oauth/google";
-import { CssBaseline, ThemeProvider, createTheme } from "@mui/material";
-
-const theme = createTheme({
-  palette: {
-    primary: { main: "#1976d2" },
-    secondary: { main: "#f50057" },
-  },
-  typography: {
-    fontFamily: "Roboto, sans-serif",
-  },
-});
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import theme from "./theme/theme";
 
 createRoot(document.getElementById("root")!).render(
   <GoogleOAuthProvider clientId="971005664793-temc8560umpfanormjketp91k35sbj6g.apps.googleusercontent.com">
